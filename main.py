26330b6b6f31a9822981a1de6638899acee8d413
besa-vinceadrian <vinceadrianbesa@gmail.com>
2025-06-06 23:26:56 +0800

    fix name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello, I'm Zcintilla R. Serquina.")
 print("Hello, I'm Michael Rua S. Maestre")
 print("Hello, I'm Rica Genevive S. Corpuz - BLACK AMERICAN")
-print("Hillu, I'm Bins Adrian A. Bisa")
+print("Hello, I'm Vince Adrian A. Besa - I'm not racist")
