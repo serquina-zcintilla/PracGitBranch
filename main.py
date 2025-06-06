7a7999e5638bb162858767ca4379065afcc066a0
besa-vinceadrian <vinceadrianbesa@gmail.com>
2025-06-06 23:21:08 +0800

    add besa name

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello, I'm Zcintilla R. Serquina.")
 print("Hello, I'm Michael Rua S. Maestre")
 print("Hello, I'm Rica Genevive S. Corpuz - BLACK AMERICAN")
+print("Hillu, I'm Bins Adrian A. Bisa")
