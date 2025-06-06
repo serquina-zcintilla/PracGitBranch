63508632e91cc9d5b5112a54c032efbf5d5dcc6d
Vince Adrian Besa <154306865+besa-vinceadrian@users.noreply.github.com>
2025-06-06 23:29:18 +0800

    add besa name (#7)
    * add besa name
    
    * fix name
    
    * add text

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello, I'm Zcintilla R. Serquina.")
 print("Hello, I'm Michael Rua S. Maestre")
 print("Hello, I'm Rica Genevive S. Corpuz - BLACK AMERICAN")
+print("Hello, I'm Vince Adrian A. Besa - I'm not racist")
